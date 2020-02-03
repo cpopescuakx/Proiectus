0b302eede4fd0fb3f3be0792de3f165f12b84ed1
cpopescuakx <58787303+cpopescuakx@users.noreply.github.com>
2020-02-03 16:50:28 +0100

    booty
    asd

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,143 @@
+<div id="menu" class="closed">
+    <!-- Inicio sidenav -->
+    <nav id="sidenav" class="bg-primary">
+        <ul class="nav flex-column">
+            <!-- Logo -->
+            <li class="nav-item">
+                <a class="navbar-brand" href="">
+                    <img src="/img/icono_light.png" width="30" height="30" class="d-inline-block align-top" alt="" href="index.php">
+                    <span class="text-light text-uppercase">Proiectus</span>
+                </a>
+            </li>
+            <!-- Fin logo -->
+
+            <!-- Item sidenav -->
+            <li class="nav-item">
+                <a href="">
+                    <i class="material-icons text-light">dashboard</i>
+                    <span class="text-light">Dashboard</span>
+                </a>
+            </li>
+            <!-- Fin item sidenav -->
+
+            <!-- Item sidenav -->
+            <li class="nav-item">
+                <a href="g3_empleats.php">
+                    <i class="material-icons text-light">person</i>
+                    <span class="text-light">Users</span>
+                </a>
+
+
+                <div class="sub-menu">
+                    <!-- Inicio Submenu -->
+                    <ul class="nav flex-column">
+                        <!-- Submenu -->
+                        <li class="nav-item">
+                            <a href="">
+                                <span class="text-light">Element 1</span>
+                            </a>
+                        </li>
+                        <!-- Fin submenu -->
+
+                        <!-- Submenu -->
+                        <li class="nav-item">
+                            <a href="">
+                                <span class="text-light">Element 2</span>
+                            </a>
+                        </li>
+                        <!-- Fin submenu -->
+
+                        <!-- Submenu -->
+                        <li class="nav-item">
+                            <a href="">
+                                <span class="text-light">Element 3</span>
+                            </a>
+                        </li>
+                        <!-- Fin submenu -->
+                    </ul>
+                </div>
+            </li>
+            <!-- Fin item sidenav -->
+
+            <!-- Item sidenav -->
+            <li class="nav-item">
+                <a class="dropdown">
+                    <i class="material-icons text-light">file_copy</i>
+                    <span class="text-light">Proposals</span>
+                    <i class="dropdown-ico material-icons text-light">keyboard_arrow_down</i>
+                </a>
+
+                <div class="sub-menu">
+                  <ul class="nav flex-column">
+                    <!-- Submenu -->
+                    <li class="nav-item">
+                        <a href="g3_llistar_propostes.php">
+
+                            <span class="text-light">All Proposals</span>
+                        </a>
+                    </li>
+                    <!-- Fin submenu -->
+
+                    <!-- Submenu -->
+                    <li class="nav-item">
+                        <a href="g3_propostes.php">
+                            <span class="text-light">My Proposals</span>
+                        </a>
+                    </li>
+                    <!-- Fin submenu -->
+                  </ul>
+                </div>
+
+            </li>
+            <!-- Fin item sidenav -->
+
+            <!-- Item sidenav -->
+            <li class="nav-item">
+                <a href="g2_llistar_projectes.php">
+                    <i class="material-icons text-light">assignment</i>
+                    <span class="text-light">Projects</span>
+                </a>
+            </li>
+            <!-- Fin item sidenav -->
+            <!-- Item sidenav -->
+            <li class="nav-item">
+                <a class="dropdown">
+                    <i class="material-icons text-light">person</i>
+                    <span class="text-light">Els meus blogs</span>
+                    <i class="dropdown-ico material-icons text-light">keyboard_arrow_down</i>
+                </a>
+
+
+               
+            </li>
+            <!-- Fin item sidenav -->
+        </ul>
+    </nav>
+    <!-- Fin sidenav -->
+
+    <!-- Inicio menu -->
+    <nav id="top-menu" class="navbar navbar-expand-lg navbar-light fixed-top bg-white">
+        <!-- Inicio toggle menu -->
+        <div class="navbar-header">
+            <i id="sidenav-toggle" class="material-icons">menu</i>
+        </div>
+        <!-- Fin toggle menu -->
+
+        <!-- Inicio elementos menu -->
+        <ul class="navbar-nav">
+            <!-- Elemento menu -->
+            <li class="nav-item active">
+                <a class="nav-link" class="text-ternari" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <!-- Fin elemento menu -->
+
+            <!-- Elemento menu -->
+            <li class="nav-item">
+                <a class="nav-link" href="">Features</a>
+            </li>
+            <!-- Fin elemento menu -->
+        </ul>
+        <!-- Fin elementos menu -->
+    </nav>
+    <!-- Fin menu -->
+</div>
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,12 +1,102 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.default')
+
+@section('content');
+<body id="body">
+    <div class="content formulari closed">
+
+        <form class="was-validated" action='{{ route('projects.store') }}' method='POST'>
+            <!-- inicio formulario -->
+            <div class="row justify-content-center">
+
+                <!-- grid container formulario-->
+                <div class="col-11 col-sm-11 col-md-10 col-lg-10 col-xl-10">
+
+                    <!-- Imagen formulario -->
+                    <div class="container">
+                        <div class="contact-image text-center mt-3">
+                            <img class="form-img" src="img/icono_negro.png" />
+                        </div>
+                    </div>
+
+                    <!-- container formulario -->
+                    <div class="container contact-form">
+
+
+                        <div class="container">
+                            <div class="row no-gutters justify-content-center mt-5">
+                                <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                    <h1>Proiectus forms.</h1>
+                                    <p>Estás en la plantilla proiectus forms, toma lo que sea útil para tu formulario, aplícalo bien, haz tu trabajo.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Input básico -->
+                        <div class="form-group mt-4">
+                            <div class="row justify-content-center">
+                                <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                    <label for="exampleFormControlInput1">Tipo de información</label>
+                                    <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Ejemplo del tipo de información" required>
+                                    <div class="invalid-feedback">Example invalid custom file feedback</div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- fin input básico -->
+                        <!-- Input básico -->
+                        <div class="form-group mt-4">
+                            <div class="row justify-content-center">
+                                <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                    <label for="exampleFormControlInput1">Tipo de información</label>
+                                    <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Ejemplo del tipo de información" required>
+                                    <div class="invalid-feedback">Example invalid custom file feedback</div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- fin input básico -->
+                        <!-- Input básico -->
+                        <div class="form-group mt-4">
+                            <div class="row justify-content-center">
+                                <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                    <label for="exampleFormControlInput1">Tipo de información</label>
+                                    <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Ejemplo del tipo de información" required>
+                                    <div class="invalid-feedback">Example invalid custom file feedback</div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- fin input básico -->
+                        <!-- Input básico -->
+                        <div class="form-group mt-4">
+                            <div class="row justify-content-center">
+                                <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                    <label for="exampleFormControlInput1">Tipo de información</label>
+                                    <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Ejemplo del tipo de información" required>
+                                    <div class="invalid-feedback">Example invalid custom file feedback</div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- fin input básico -->
+
+                    </div>
+                    <!-- fin container formulario -->
+
+                </div>
+                <!--  fin grid container formulario-->
+
+            </div>
+            <!-- fin formulario -->
+        </form>
+
+    </div>
+
+    <script src="js/header.js"></script>
+
+</body>
+@stop
+{{-- <body>
     <h1>Crear un nou projecte</h1>
 
 <form action='{{ route('projects.store') }}' method='POST'>
@@ -15,6 +105,9 @@
           <td><input type="text" name="name"></td>
         </tr>
         <br>
+        <div class="row">
+            <div></div>
+        </div>
         <tr><td>Pressupost:</td><br>
           <td><input type="text" name="pro_family"></td>
         </tr>
@@ -29,5 +122,5 @@
         <br>
         <input name='crear' value='Crear' type='submit'/>
     </form>
-</body>
-</html>
+</body> --}}
+
