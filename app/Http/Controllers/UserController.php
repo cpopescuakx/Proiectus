fe24bb05545bb03925b88f5bd9ea09ee2bd69ec8
Xavier Naves <xavinaves@gmail.com>
2020-02-10 16:48:44 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -259,4 +259,110 @@ class UserController extends Controller
     {
         //
     }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function indexManager()
+    {
+        $data['users'] = User::orderBy('id', 'desc')->paginate(10);
+        return view('user.index', $data);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createManager()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function storeManager(Request $request)
+    {
+        $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'name' => 'required',
+            'email' => 'required',
+            'dni' => 'required',
+            'password' => 'required',
+        ]);
+
+        User::create($request->all());
+
+        return redirect()->route('user.index')
+                         ->with('funciona');
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function showManager(User $user)
+    {
+        return view('user.show',compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function editManager(User $user)
+    {
+        return view('user.edit',compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateManager(Request $request, User $user)
+    {
+        $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'name' => 'required',
+            'email' => 'required',
+            'dni' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user->update($request->all());
+
+        return redirect()->route('user.index')
+                         ->with('success');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroyManager(User $user)
+    {
+        $user->delete();
+
+        return redirect()->route('user.index')
+                         ->with('success');
+    }
 }
