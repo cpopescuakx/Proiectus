712c6744d9137badeb357c1ff0088d78914c90cc
rohen21 <svyatossemenyuk@iesmontsia.org>
2020-02-10 21:01:15 +0100

    CompanyController G1
    CompanyController G1

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Company; //afegit
+use Redirect; //afegit
+use PDF; //afegit
 
 class CompanyController extends Controller
 {
@@ -85,6 +88,19 @@ class CompanyController extends Controller
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
     //GRUP1 ACCIONS
     /**
      * Display a listing of the resource.
@@ -93,6 +109,104 @@ class CompanyController extends Controller
      */
     public function indexCompany()
     {
-        
+      $data['companies'] = Company::orderBy('id_company','desc')->paginate(10);
+      return view('company.list',$data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createCompany()
+    {
+        return view('company.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function storeCompany(Request $request)
+    {
+        // POSAR ELS CAMPS DE LA TAULA OBLIGATORIS
+        $request->validate([
+            'email' => 'required',
+            'name' => 'required',
+            'nif' => 'required',
+            'sector' => 'required',
+            'status' => 'active',
+        ]);
+
+        Company::create($request->all());
+
+        return Redirect::to('companies')
+       ->with('Éxit! L empresa s ha creat correctament!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function showCompany($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function editCompany($id)
+    {
+        $where = array('id_company' => $id);
+        $data['company_info'] = Company::where($where)->first();
+
+        return view('company.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateCompany(Request $request,Company $id)
+    {
+        $request->validate([
+            'email' => 'required',
+            'name' => 'required',
+            'nif' => 'required',
+            'sector' => 'required',
+            'status' => 'required',
+        ]);
+
+
+         Company::findOrFail($id)->first()->fill($request->all())->save();
+         //Company::find($request->id)->update($request->all());
+         return redirect()->route('company.index')
+                          ->with('Éxit','L empresa s ha modificat correctament!');
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroyCompany($id)
+    {
+        Company::where('id_company',$id)->delete();
+
+        return Redirect::to('companies')->with('Éxit','L empresa s ha eliminat correctament!');
     }
 }
