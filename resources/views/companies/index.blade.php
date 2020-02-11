7c127a9aee87a36ce570b9e4eb4fb8618e7964f6
rohen21 <svyatossemenyuk@iesmontsia.org>
2020-02-11 13:21:24 +0100

    Update index.blade.php

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -34,9 +34,9 @@
                  <!--<td>{{ $company->id_city }}</td>-->
                  <td>{{ $company->status }}</td>
                  <!--<td>{{ date('Y-m-d', strtotime($company->created_at)) }}</td>-->
-                 <td><a href="{{ route('companies.editCompanies',$company->id_company)}}" class="btn btn-primary">EDITA</a></td>
+                 <td><a href="{{ route('companies.edit',$company->id_company)}}" class="btn btn-primary">EDITA</a></td>
                  <td>
-                 <form action="{{ route('companies.destroyCompanies', $company->id_company)}}" method="post">
+                 <form action="{{ route('companies.destroy', $company->id_company)}}" method="post">
                   {{ csrf_field() }}
                   @method('DELETE')
                   <button class="btn btn-danger" type="submit">ELIMINA</button>
