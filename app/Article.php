d11340957be0de39a67883c33548f742148b7612
xbd1999 <58074031+xbd1999@users.noreply.github.com>
2020-02-11 17:17:24 +0100

    Revert "Revert "Revert "a"""
    This reverts commit d72fb72068c83663ebb2b54b54cd37dcfc09d255.

--- app/Article.php
+++ app/Article.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-  protected $fillable = [
-    'id_article', 'id_project', 'version', 'title', 'content', 'creation_date', 'reference', 'id_user', 'status'
-  ];
-  protected $primaryKey = 'id_project';
+
+    //
+
 }
