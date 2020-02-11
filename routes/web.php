931a2c0468bfd0bdf44bc51f08d8fcb5a5c6d007
xbd1999 <58074031+xbd1999@users.noreply.github.com>
2020-02-11 17:12:32 +0100

    routes
    routes

--- routes/web.php
+++ routes/web.php
@@ -97,7 +97,14 @@ Route::get('wiki/{id_project}', [
 ]);
 
 
-/** Rutes per a l'apartat de la wiki */
+/** Rutes per a l'apartat de la gestio dels articles de la wiki */
 
+Route::resource('articles', 'ArticleController');
+
+
+Route::get('wiki/{id_project}', [
+    'Middleware' => 'auth',
+    'uses' => 'ArticleController@index'
+]);
 
 /** Rutes per a l'apartat de perfils d'usuari */
