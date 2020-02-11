81c5ab8d52688184d0c2250333ef60fed9469113
xbd1999 <58074031+xbd1999@users.noreply.github.com>
2020-02-11 17:14:43 +0100

    Revert "a"
    This reverts commit e4f9a2b7edb0e4bbe54fca23bcecae0a22e1f6e1.

--- routes/web.php
+++ routes/web.php
@@ -87,15 +87,6 @@ Route::get('blog/{id_project}/post/{id_post}/edit', [
     'uses' => 'PostController@edit'
 ]);
 
-/** Rutes per a l'apartat de la gestio dels articles de la wiki */
-
-Route::resource('articles', 'ArticleController');
-
-Route::get('wiki/{id_project}', [
-    'Middleware' => 'auth',
-    'uses' => 'ArticleController@index'
-]);
-
 
 /** Rutes per a l'apartat de la gestio dels articles de la wiki */
 
@@ -108,3 +99,4 @@ Route::get('wiki/{id_project}', [
 ]);
 
 /** Rutes per a l'apartat de perfils d'usuari */
+
