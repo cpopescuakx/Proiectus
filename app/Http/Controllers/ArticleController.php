9d6a93e77120bca4dad403a0a6674a33940811b9
Aleix <aleix.esteve.gisbert@gmail.com>
2020-02-11 17:05:14 +0100

    Revert "Merge branch 'grup4_v2' of https://github.com/cpopescuakx/Proiectus into grup4_v2"
    This reverts commit e4a83410c1bc8f2bd48f605e6d2f03430f15fd24, reversing
    changes made to fea983783752fe821cde3a27e9014367aac5f377.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -12,15 +11,9 @@ class ArticleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($id_project)
+    public function index()
     {
-        $articles = Article::all()
-        ->where('id_project', '=', $id_project);
-
-
-
-
-        return view('articles.index', compact('username','articles'));
+        //
     }
 
     /**
@@ -30,7 +23,7 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        return view('articles.create');
+        //
     }
 
     /**
@@ -41,83 +34,51 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            //'id_article' => 'required',
-            'id_project' => 'required', // assi
-            'version' => 'required',
-            'title' => 'required',
-            'content' => 'required',
-            'creation_date' => 'required',
-            'reference' => 'required',
-            'id_user' => 'required',
-            'status' => 'required',
-        ]);
-
-        Article::create($request->all());
-
-        return redirect()->route('articles.index')
-                        ->with('success','Article created successfully.');
+        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Article  $article
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Article $article)
+    public function show($id)
     {
-        return view('articles.show',compact('article'));
+        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Article  $article
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Article $id_article)
+    public function edit($id)
     {
-        $article = Article::where('id_article', $id_article)->first();
-        return view('articles.edit',compact('article'));
+        //
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Article  $article
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id_article)
+    public function update(Request $request, $id)
     {
-        $validatedData = $request->validate([
-                  'id_article' => 'required',
-                  'id_project' => 'required',
-                  'version' => 'required',
-                  'title' => 'required',
-                  'content' => 'required',
-                  'creation_date' => 'required',
-                  'reference' => 'required',
-                  'id_user' => 'required',
-                  'status' => 'required',
-              ]);
-              Article::whereId($id_article)->update($validatedData);
-
-              return redirect()->route('articles.index')->with('success', 'Article updated successfully');
+        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Article  $article
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Article $article)
+    public function destroy($id)
     {
-        $article->delete();
-
-        return redirect()->route('articles.index')
-                        ->with('success','Article deleted successfully');
+        //
     }
 }
