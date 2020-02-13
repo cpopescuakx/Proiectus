e1e912dff9b35f25afdd652788d7492b0d13aaca
Andrei-B <andreibordeianu2@iesmontsia.org>
2020-02-13 16:22:17 +0100

    gf

--- android/app/src/main/java/com/example/proiectus/LlistaInstituts.java
+++ android/app/src/main/java/com/example/proiectus/LlistaInstituts.java
@@ -11,4 +11,21 @@ public class LlistaInstituts {
         array.add(i);
     }
 
+    public void dadesProva(){
+        Institut ins1 = new Institut("IES Montsià", "C012345", "Amposta");
+        Institut ins2 = new Institut("Ramon Berenguer IV", "C345713", "Amposta");
+        Institut ins3 = new Institut("IES Tecnificació", "C561004", "Amposta");
+        Institut ins4 = new Institut("IES Camarles", "C092773", "Camarles");
+
+        array.add(ins1);
+        array.add(ins2);
+        array.add(ins3);
+        array.add(ins4);
+    }
+
+    public void modificarInstitut (int id, String nom, String codi, String ciutat){
+
+
+    }
+
 }
--- android/app/src/main/java/com/example/proiectus/PaginaPrincipal.java
+++ android/app/src/main/java/com/example/proiectus/PaginaPrincipal.java
@@ -25,4 +25,6 @@ public class PaginaPrincipal extends Activity {
         startActivity(i);
     }
 
+    LlistaInstituts ll = new LlistaInstituts();
+
 }
