3139ee9a5184e6302b9c578a9784c8343600b7cb
cpopescuakx <58787303+cpopescuakx@users.noreply.github.com>
2020-02-17 16:27:39 +0100

    ADB ES BRUTAL

---
+++ android/app/src/main/java/com/example/proiectus/EliminarInstitut.java
@@ -0,0 +1,14 @@
+package com.example.proiectus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EliminarInstitut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar_institut);
+    }
+}
