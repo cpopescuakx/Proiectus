71cc3c5976e9532f2fa5ac41729e8597c4348bba
cpopescuakx <58787303+cpopescuakx@users.noreply.github.com>
2020-02-17 17:21:39 +0100

    asd supermerda

---
+++ android/app/src/main/java/com/example/proiectus/LlistarInstituts.java
@@ -0,0 +1,14 @@
+package com.example.proiectus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LlistarInstituts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llistar_instituts);
+    }
+}
