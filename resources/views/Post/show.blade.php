9678bfa2b698205a3fd601ec7ed9f0e8ab8f550d
aleixBY23 <aleix.esteve.gisbert@gmail.com>
2020-02-18 15:40:13 +0100

    Show de Post

--- resources/views/Post/show.blade.php
+++ resources/views/Post/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
-<div
+<div>
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="#">Projecte</a></li>
@@ -9,20 +9,19 @@
   </ol>
 </nav>
 </div>
-
-<div class="card-body">
+<div class="container mb-5">
+<footer class="blockquote-footer float-right"> Creat per <a href=""> <cite title="Source Title"> {{$post->id_user}}</cite></a> el {{$post->created_at}}</footer>
+    <br>
+    <br>
     <a class="float-right" href="{{$post->id_post}}/edit"><i style="font-size: 1rem" class="material-icons">edit</i></a>
     <a class="float-right" href="{{$post->id_post}}/destroy" ><i style="font-size: 1rem" class="material-icons">delete</i></a>
 
-        <h5 class="card-title">{{$post->title}}</h5>
-    </a>
+        <h5 class="card-title border-bottom">{{$post->title}}</h5>
     <div class="">
         <p class="card-text">{!!$post->content!!}</p>
 
     </div>
+    
 </div>
-
-    <footer class="blockquote-footer float-right"> Creat per <a href=""> <cite title="Source Title"> {{$post->id_user}}</cite></a> el {{$post->created_at}}</footer>
-
 
 @endsection
