210faadc553ce0740447a91be2c777b42ac7e12a
jbarbera99 <59975313+jbarbera99@users.noreply.github.com>
2020-03-24 19:44:47 +0100

    Canvis

--- app/Article.php
+++ app/Article.php
@@ -2,11 +2,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Feed\Feedable;
+use Spatie\Feed\FeedItem;
 
-class Article extends Model
+class Article extends Model Implements Feedable
 {
   protected $fillable = [
     'id_article', 'id_project', 'version', 'title', 'content', 'creation_date', 'reference', 'id_user', 'status'
   ];
   protected $primaryKey = 'id_article';
+
+  /**
+   * 
+   */
+  public function toFeedItem()
+    {
+      return FeedItem::create([
+        'id' => $this->id_article,
+        'title' => $this->title,
+        'summary' => $this->content,
+        'updated' => $this->updated_at,
+        'link' => $this->status,
+        'author' => $this->id_user,
+      ]);
+    }
+    public static function getAllFeedItems()
+    {
+       return Article::all();
+    }
 }
