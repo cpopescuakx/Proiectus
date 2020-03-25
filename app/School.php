adf58db159550de7065dc2f2b7b49ac6fcb8c0f2
Didac3 <57676273+Didac3@users.noreply.github.com>
2020-03-25 15:46:44 +0100

    feedgrup1

--- app/School.php
+++ app/School.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Feed\Feedable;
+use Spatie\Feed\FeedItem;
 
-class School extends Model
+class School extends Model Implements Feedable
 {
     protected $primaryKey = 'id_school';
+
     protected $fillable = [
      'email',
      'name',
@@ -14,4 +17,29 @@ class School extends Model
      'type',
 
     ];
+
+    public function toFeedItem()
+    {
+      return FeedItem::create([
+        // 'id_post' => $this->id_post,
+        // 'id_project' => $this->id_project,
+        // 'title' => $this->title,
+        // 'content' => $this->content,
+        // 'id_user' => $this->id_user,
+        // 'status' => $this->status,
+        // 'timestamps' => $this->timestamps,
+        // 'creation_date' => $this->creation_date,
+        // 'update_at' => $this->update_at,
+        'id' => $this->id_school,
+        'title' => $this->name,
+        'summary' => $this->address,
+        'updated' => $this->updated_at,
+        'link' => $this->status,
+        'author' => $this->id_city,
+      ]);
+    }
+    public static function getAllFeedItems()
+    {
+       return School::all();
+    }
 }
