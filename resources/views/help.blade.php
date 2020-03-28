7a0eb68c9f5b71594cc03fc1cf91be3a6a047d60
Didac3 <57676273+Didac3@users.noreply.github.com>
2020-03-28 14:01:53 +0100

    update help.php

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/components/g2/g2_style.css')}}">
     <div class="container ">
         <div class="row justify-content-center">
-            <div class="column mt-4 mb-4">
+            <div class="column mt-4 mb-4"><br>
                 <h1 class="display-4">Tutorials d'ajuda</h1>
             </div>
         </div>
@@ -24,18 +24,63 @@
     </div>
 
     <div class="row justify-content-center">
-        <div id="gestor" class="tabcontent mt-3">
+        <div id="gestor" class="tabcontent mt-3 col-md 6">
             <h2 class="mt-3 justify-content-center"><strong>Gestió Gestors</strong></h2>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/zD7I-GbjJ_w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="progress-container col-md-6">
+              <div class="progress-bar" id="myBar"></div>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/exQfi015i4o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          <div class="content col-md-6">
+          <p>We have created a "progress bar" to <b>show how far a page has been scrolled</b>.</p>
+          <p>It also <b>works when you scroll back up</b>.</p>
+          <p>It is even <b>responsive</b>! Resize the browser window to see the effect.</p>
+          <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+          <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        </div>
+            <style>
+            .progress-container {
+              width: 100%;
+              height: 8px;
+            }
+            .content {
+              margin: 10px auto 0 auto;
+              width: 80%;
+            }
+            </style>
         </div>
     </div>
 
+
+
     <div class="row justify-content-center">
-        <div id="wiki_blog" class="tabcontent mt-3">
-            <h2 class="mt-3"><strong>Gestió wiki i blog</strong></h2>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/zD7I-GbjJ_w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
+        <div id="wiki_blog" class="tabcontent mt-3 col-md 6">
+            <h2 class="mt-3 justify-content-center"><strong>Gestió wiki i blog</strong></h2>
+            <div class="progress-container col-md-6">
+              <div class="progress-bar" id="myBar"></div>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/zD7I-GbjJ_w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+              <div class="content col-md-6">
+              <p>We have created a "progress bar" to <b>show how far a page has been scrolled</b>.</p>
+              <p>It also <b>works when you scroll back up</b>.</p>
+              <p>It is even <b>responsive</b>! Resize the browser window to see the effect.</p>
+              <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+              <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+            </div>
+                <style>
+                .progress-container {
+                  width: 100%;
+                  height: 8px;
+                }
+                .content {
+                  margin: 10px auto 0 auto;
+                  width: 80%;
+                }
+                </style>
+            </div>
+
     </div>
+
+
     <script>
         document.getElementById('gestor').style.display="block";
         function tabs(evt, apartat) {
