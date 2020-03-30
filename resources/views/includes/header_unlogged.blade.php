93446cca7562ca2f2ec2379c6bef67ebfbb8ef68
cmasana <masana.ou@gmail.com>
2020-03-30 22:17:06 +0200

    Landing Page: Dropdown added
    Añadido desplegable con Logout en barra de navegación superior

--- resources/views/includes/header_unlogged.blade.php
+++ resources/views/includes/header_unlogged.blade.php
@@ -36,13 +36,28 @@
             <!-- Fin elemento menu -->
 
             <!-- Elemento menu -->
-            @if(Auth::guest())
+            @guest
             <li class="nav-item ml-3">
                 <a type="button" class="btn btn-outline-primary"  href="login">Inicia sessió<span class="sr-only">(current)</span></a>
             </li>
             @else
-            <li class="nav-item ml-3 mt-2">
-               <a>{{ auth()->user()->username }} </a>
+            <li class="nav-item ml-3">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                   {{ Auth::user()->username }}
+                </a>
+
+                <!-- Desplegable con Logout -->
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
             </li>
             <!-- Fin elemento menu -->
             @endif
