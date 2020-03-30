526caa83a51638eef13c8e3db70937bf4e76dd05
cpopescuakx <58787303+cpopescuakx@users.noreply.github.com>
2020-03-30 17:15:23 +0200

    asd

--- resources/views/includes/footer-scripts.blade.php
+++ resources/views/includes/footer-scripts.blade.php
@@ -12,5 +12,3 @@
 <script src="{{asset('bootstrap-datepicker-1.9.0-dist/js/bootstrap-datepicker.js')}}"></script>
 <script src="{{asset('bootstrap-datepicker-1.9.0-dist/locales/bootstrap-datepicker.ca.min.js')}}"></script>
 <script src="{{ asset('js/g2/students.create.js') }}"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBneN0MIKQ-fLajTh_7oq-JiX_1w9ApMB8&callback=initMap"></script>
-async defer></script>
