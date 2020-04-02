8c82aea3f70a2577ae428d7448324b5ee6f7ebce
cmasana <masana.ou@gmail.com>
2020-04-02 04:37:54 +0200

    Update UserController.php
    Cambio de la redirección cuando desactivamos un manager

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -85,7 +85,7 @@ class UserController extends Controller
         $user->status = 'inactive';
         $user->save();
 
-        return redirect()->route('index.index');
+        return redirect()->back();
 
     }
 
