4ce64032b67756960365957bf0f7ef2adb6b3d9c
cmasana <masana.ou@gmail.com>
2020-04-02 04:28:51 +0200

    Update Routes
    Se ha añadido una ruta nueva para la funcionalidad de activar un manager que se encontraba inactivo

--- routes/web.php
+++ routes/web.php
@@ -158,7 +158,8 @@ Route::middleware(['CheckRole'])->group(function () {
     Route::get('managerProfile/{id}', 'UserController@indexProfile')->name('managers.indexP1');
     Route::get('managerProfile/{id}/edit', 'UserController@editProfile')->name('managers.editP');
     Route::post('managerProfile/{id}/update', 'UserController@updateProfile')->name('managers.updateP');
-    Route::delete('managerProfile/{id}/delete', 'UserController@destroyProfile')->name('managers.destroyP');
+    Route::get('managerProfile/{id}/delete', 'UserController@destroyProfile')->name('managers.destroyP');
+    Route::get('managerProfile/{id}/active', 'UserController@activeProfile')->name('managers.activeP');
 
     Route::get('managers', 'UserController@indexManager')->name('managers.index');
     Route::get('managers/create', 'UserController@createManager')->name('managers.create');
