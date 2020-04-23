54556fc512dc13a815385d4ca741d19443730a4d
cmasana <masana.ou@gmail.com>
2020-04-23 17:08:02 +0200

    Proposta individual
    Cambio de estructura y estilos

--- resources/views/proposals/show.blade.php
+++ resources/views/proposals/show.blade.php
@@ -2,38 +2,70 @@
 
 @section('content')
 
-    <!-- <link rel="stylesheet" type="text/css" href="{{asset('css/components/g2/g2_style.css')}}"> -->
-    <a style="color: #007bff" onclick="AJAXJSPur();">a<i style="font-size: 1.3rem" class="fas fa-sync" alt="Icona per a modificar"></i></a>
-    <a style="color: #007bff" onclick="AJAXjQuery();">b<i style="font-size: 1.3rem" class="fas fa-sync" alt="Icona per a modificar"></i></a>
-    <a style="color: #007bff" onclick="AJAXJSFetch();">v<i style="font-size: 1.3rem" class="fas fa-sync" alt="Icona per a modificar"></i></a>
-    <div class="container ">
-        <div class="row justify-content-center">
-            <div class="column mt-4 mb-4">
-                <h1>Títol: {{$proposal->name}}</h1>
-            </div>
-        </div>
-    </div>
-    <div class="container ">
-        <div class="row justify-content-center">
-            <div class="column mt-4 mb-4">
-                <h3>Descripció: {{$proposal->description}}</h3>
-            </div>
-        </div>
-    </div>
-    <div class="container ">
-        <div class="row justify-content-center">
-            <div class="column mt-4 mb-4">
-                <h3>Familia professional: {{$proposal->professional_family}}</h3>
-            </div>
-        </div>
-    </div>
-    <div class="container ">
-        <div class="row justify-content-center">
-            <div class="column mt-4 mb-4">
-                <h3>Categoria: {{$proposal->category}}</h3>
-            </div>
-        </div>
-    </div>
-    <script src="{{asset('js/AJAXCezar.js')}}"></script>
+    <div class="container pt-5">
 
-@stop
+        <!-- Encabezado con logo y título -->
+        <div class="row p-5 shadow">
+            <div class="col my-auto">
+                @if(Auth::user()->logo_entity == 'null')
+                    <img class="rounded-circle mr-auto img-fluid img-thumbnail" alt="250x250" src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" data-holder-rendered="true" max-width="50%" />
+                @else 
+            <img class="rounded-circle mr-auto" alt="250x250" src="{{ Auth::user()->logo_entity }}" data-holder-rendered="true" />
+                @endif
+            </div>
+            <div class="col my-auto">
+                <h1 class="my-5">{{ $proposal->name }}</h1>
+            </div>
+            <div class="col my-auto">
+                <a class="btn btn-dark text-white">APPLY</a>
+            </div>
+        </div>
+        <!-- /row -->
+        <br>
+        <!-- Espacio para tags -->
+        <div class="row p-5 shadow">
+            <div class="col my-auto">
+                             
+            </div>
+            <div class="col my-auto">
+                
+            </div>
+            <div class="col my-auto">
+                
+            </div>
+        </div>
+        <!-- /row -->
+        <br>
+        <!-- Características propuestas -->
+        <div class="row p-5 shadow">
+            <div class="col my-auto">
+            </div>
+            <div class="col my-auto">
+            </div>
+            <div class="col my-auto">
+            </div>
+        </div>
+        <!-- /row -->
+        <br>
+        <!-- Otras cosas xD -->
+        <div class="row p-5 shadow">
+            <div class="col my-auto">
+            </div>
+            <div class="col my-auto">
+            </div>
+            <div class="col my-auto">
+            </div>
+        </div>
+        <!-- /row -->
+    </div>
+
+
+@endsection
+
+<!--
+
+{{$proposal->description}}
+{{$proposal->professional_family}}
+{{$proposal->category}}
+
+-->
