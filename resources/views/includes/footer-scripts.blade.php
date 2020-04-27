928ab5410d402c3d4db98f09918ccf1fced4e52a
cmasana <masana.ou@gmail.com>
2020-04-27 19:00:37 +0200

    Bug Dropdown Arreglado
    He puesto comentarios para que no la volváis a liar. Había 2 versiones de JQUERY

--- resources/views/includes/footer-scripts.blade.php
+++ resources/views/includes/footer-scripts.blade.php
@@ -1,25 +1,37 @@
 <!-- Bootstrap core JavaScript
 ================================================= -->
-<!-- Placed at the end of the document so the pages load faster -->
+<!-- Bootstrap JS -->
 <script src="{{ asset('bootstrap-select-1.13.9/dist/js/bootstrap-select.js') }}"></script>
 <script src="{{ asset('bootstrap-table-master/dist/bootstrap-table.js') }}"></script>
+<script src="{{ asset('bootstrap-datepicker-1.9.0-dist/js/bootstrap-datepicker.js') }}"></script>
+<script src="{{ asset('bootstrap-datepicker-1.9.0-dist/locales/bootstrap-datepicker.ca.min.js') }}"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="{{ asset('js/bootstrap.js') }}"></script>
+
+<!-- TETHER -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+
+<!-- JQUERY ESTÁ AQUÍ -->
 <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+
+<!-- POPPER -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+<!-- GOOGLE MAPS API -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBneN0MIKQ-fLajTh_7oq-JiX_1w9ApMB8&callback=initMap&libraries=places"></script>
+
+<!-- CUSTOM JS -->
 <script src="{{ asset('js/header.js') }}"></script>
 <script src="{{ asset('js/indexEmployee.js') }}"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBneN0MIKQ-fLajTh_7oq-JiX_1w9ApMB8&callback=initMap&libraries=places"></script>
-<script src="{{asset('bootstrap-datepicker-1.9.0-dist/js/bootstrap-datepicker.js')}}"></script>
-<script src="{{asset('bootstrap-datepicker-1.9.0-dist/locales/bootstrap-datepicker.ca.min.js')}}"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="{{ asset('js/carousel.js') }}"></script>
+
+<!-- SLICK, LIBRERÍA PARA CARRUSEL -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
 
 <!-- Scripts header -->
-<!-- <script src="{{ asset('js/jqueryH.min.js') }}"></script> -->
 <script src="{{ asset('js/popperH.js') }}"></script>
 <script src="{{ asset('js/bootstrapH.min.js') }}"></script>
 <script src="{{ asset('js/mainH.js') }}"></script>
 
-<!-- Scripts notificacions -->
-<!-- <script src="{{ asset('js/notificacions.js') }}"></script> -->
+<!-- Scripts summernote -->
+<script src="{{ asset('summernote-master/dist/summernote-bs4.min.js') }}"></script>
