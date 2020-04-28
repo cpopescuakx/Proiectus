fb82ae853981bb5bb159f40d132d7d42055b0949
cmasana <masana.ou@gmail.com>
2020-04-28 06:10:17 +0200

    Update footer-scripts.blade.php

--- resources/views/includes/footer-scripts.blade.php
+++ resources/views/includes/footer-scripts.blade.php
@@ -2,7 +2,13 @@
 ================================================= -->
 
 <!-- JQUERY ESTÁ AQUÍ -->
+
 <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+<!-- POPPER -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+<!-- SLICK, LIBRERÍA PARA CARRUSEL -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
 
 <!-- Bootstrap JS -->
 <script src="{{ asset('bootstrap-select-1.13.9/dist/js/bootstrap-select.js') }}"></script>
@@ -15,9 +21,6 @@
 <!-- TETHER -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 
-<!-- POPPER -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
 <!-- GOOGLE MAPS API -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBneN0MIKQ-fLajTh_7oq-JiX_1w9ApMB8&callback=initMap&libraries=places"></script>
 
@@ -25,9 +28,6 @@
 <script src="{{ asset('js/header.js') }}"></script>
 <script src="{{ asset('js/indexEmployee.js') }}"></script>
 <script src="{{ asset('js/carousel.js') }}"></script>
-
-<!-- SLICK, LIBRERÍA PARA CARRUSEL -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
 
 <!-- Scripts summernote -->
 <script src="{{ asset('summernote-master/dist/summernote-bs4.min.js') }}"></script>
