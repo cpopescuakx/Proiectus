913d483cb4fa5254f9e0d294d0592a79b41beb0b
cmasana <masana.ou@gmail.com>
2020-04-28 06:21:57 +0200

    Update footer-scripts.blade.php

--- resources/views/includes/footer-scripts.blade.php
+++ resources/views/includes/footer-scripts.blade.php
@@ -2,10 +2,10 @@
 ================================================= -->
 
 <!-- JQUERY ESTÁ AQUÍ -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-<script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
 <!-- POPPER -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
 <!-- SLICK, LIBRERÍA PARA CARRUSEL -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
@@ -16,7 +16,8 @@
 <script src="{{ asset('bootstrap-datepicker-1.9.0-dist/js/bootstrap-datepicker.js') }}"></script>
 <script src="{{ asset('bootstrap-datepicker-1.9.0-dist/locales/bootstrap-datepicker.ca.min.js') }}"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="{{ asset('js/bootstrap.js') }}"></script>
+
+<!-- SI se insertan los enlaces de VUE (bootstrap.js) no funcionan los dropdowns correctamente-->
 
 <!-- TETHER -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
