71574b7c112cbf45e16537d3d051bdd26efee022
cmasana <masana.ou@gmail.com>
2020-04-29 22:02:11 +0200

    Fix default.blade
    Corregido un error por el que el footer aparecía desplazado

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,22 +4,23 @@
 	</head>
 	<body>
 
-	<div class="wrapper d-flex align-items-stretch">
-		<header class="headerCSSpropi row">
-			@include('includes.header')
-		</header>
+		<div class="wrapper d-flex align-items-stretch">
+			<header class="headerCSSpropi row">
+				@include('includes.header')
+			</header>
 
-	   <div id="content" class="p-4 p-md-5 pt-5">
-	           @yield('content')
-	   </div>
-	</div>
+		<div id="content" class="p-4 p-md-5 pt-5">
+				@yield('content')
+		</div>
+		
 
-	<div id="footerBO">
-			@include('includes.footer')
-	</div>
-		@include('includes.footer-scripts') {{-- Los scripts van antes de cerrar el body --}}
+			<div id="footerBO">
+					@include('includes.footer')
+			</div>
+				@include('includes.footer-scripts') {{-- Los scripts van antes de cerrar el body --}}
 
-        <!-- THIS PAGE's CUSTOM JS-->
-        @yield('custom-scripts') {{-- yield per a incloure scripts personalitzats nomes en algunes pagines on ens interessi --}}
+				<!-- THIS PAGE's CUSTOM JS-->
+				@yield('custom-scripts') {{-- yield per a incloure scripts personalitzats nomes en algunes pagines on ens interessi --}}
+		</div>
 	</body>
 </html>
