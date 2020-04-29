5cfa54bd541761329d3020416a3db068fdefdb69
aleixBY23 <43808251+aleixBY23@users.noreply.github.com>
2020-04-29 15:33:27 +0200

    Afegir sombres a les cards del dashboard de projectes again

--- resources/views/projects/dashboard.blade.php
+++ resources/views/projects/dashboard.blade.php
@@ -7,8 +7,8 @@
         <div class="row justify-content-center">
 
                 <form class ="form-inline justify-content-center m-4" action="{{route('projects.dashboard')}}" method = "GET" >
-                    <input name = "name" class="form-control" placeholder="Buscar...">
-                    <button style="color: white;" type="submit" class="btn bg-primary1 ml-2">Buscar</button>
+                    <input name = "name" class="form-control shadow" placeholder="Buscar...">
+                    <button style="color: white;" type="submit" class="btn bg-primary1 ml-2 shadow">Buscar</button>
                 </form>
         </div>
     </div>
@@ -17,7 +17,7 @@
         <div class="row justify-content-center">
             @foreach ($projects as $project)
                 <div class="column">
-                    <div class="card m-2 shadow-sm p-3 mb-5 bg-white rounded">
+                    <div class="card m-2 shadow mb-5">
                         <img class="card-img-top" src="{{asset('img/foto_small.jpg')}}">
                         <div class="card-body">
                             <h5 style="width: 15rem;" class="text-truncate card-title">{{$project->name}}</h5>
