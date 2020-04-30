9125471ac4aace66a0a331a0c56cd70323efd1be
xbd1999 <58074031+xbd1999@users.noreply.github.com>
2020-04-30 11:57:25 +0200

    gdf

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button id="submit" disabled="false" type="submit" class="btn btn-primary">
+                                <button id="submit" type="submit" class="btn btn-primary"> <!-- disabled="true" -->
                                     {{ __('Login') }}
                                 </button>
 
