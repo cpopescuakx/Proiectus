7657a479f2a21cfa1ae7ba53e9a303739fe7d3c2
aleixBY23 <43808251+aleixBY23@users.noreply.github.com>
2020-05-04 18:24:45 +0200

    Canvis en scripts i mes fato

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -155,6 +155,47 @@
     <script src="{{asset('js/AJAXAndrei.js')}}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     @section('custom-scripts')
-        <script src="{{asset('js/WikiBlog.js')}}"></script>
+        <script>
+            $('#summernoteBlog').summernote({
+                placeholder: 'Contingut',
+                tabsize: 2,
+                height: 100,
+                minHeight: 100,
+                maxHeight: 400
+            });
+
+            /* Comprovem si el contingut del post esta buit al fer submit i
+                evitem continuar si està buit
+            */
+            $('#postCreationForm').on('submit', function(e) {
+                // Comprovem si el contingut del post esta buit
+                if ($('#summernoteBlog').summernote('isEmpty')) {
+                    console.log('Introdueix el contingut del post!');
+                    // Evitar el submit
+                    e.preventDefault();
+                }
+            });
+            $('#summernoteWiki').summernote({
+                placeholder: 'Contingut',
+                tabsize: 2,
+                height: 100,
+                minHeight: 100,
+                maxHeight: 400
+            });
+
+
+            /* Comprovem si el contingut del post esta buit al fer submit i
+                evitem continuar si està buit
+            */
+            $('#articleCreationForm').on('submit', function(e) {
+                // Comprovem si el contingut del post esta buit
+                if ($('#summernoteWiki').summernote('isEmpty')) {
+                    console.log('Introdueix el contingut del article!');
+                    // Evitar el submit
+                    e.preventDefault();
+                }
+            })
+
+        </script>
     @endsection
 @endsection
