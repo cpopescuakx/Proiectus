b2d1b49168f926bac8444f00c4a92a3783883c6b
cpopescuakx <58787303+cpopescuakx@users.noreply.github.com>
2020-05-05 16:55:26 +0200

    coses estil

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,26 +5,27 @@
 <style>
 .background {
   position: absolute;
-  top: 0;
-  left: 1;
-  margin-left: 80%;
-  margin-right: auto;
-  width: 10%;
-  height: 100%;
-  background: url(https://gifimage.net/wp-content/uploads/2018/11/pokemon-pixel-art-gif-4.gif) 50% no-repeat;
-  background-size: cover;
-  z-index: -1;
-  opacity: .5;
+      top: 0;
+      left: 1;
+      /* margin-left: 50%; */
+      /* margin-right: auto; */
+      width: 50%;
+      height: 100%;
+      background: url(https://gifimage.net/wp-content/uploads/2018/11/pokemon-pixel-art-gif-4.gif) 50% no-repeat;
+      background-size: cover;
+      z-index: -1;
+      opacity: .5;
 }
 
 h1 {
   font-size: 10rem;
 }
 </style>
-<div class="background">
 
-</div>
 <div class="container">
+  <div class="background">
+
+  </div>
   <div class="row">
     <div class="col">
       <h1>500</h1>
