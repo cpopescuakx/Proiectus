8c5729c8c9c0cbc02c30fa56d9d41e0a5a0f5a3a
jbarbera99 <59975313+jbarbera99@users.noreply.github.com>
2020-05-12 16:17:16 +0200

    Cues arreglades
    asassa

--- app/Jobs/ProcessCSV.php
+++ app/Jobs/ProcessCSV.php
@@ -91,76 +91,10 @@ class ProcessCSV implements ShouldQueue
                         case '2':
                             Log::info($this->byuser->username . ' - [ IMPORT ] - users - Nou empleat: ' . $data->username . ' inserit!');
                             break;
-                        
-                        default:
-                            break;
-                    }
-                }
-            } catch (\Illuminate\Database\QueryException $th) {
-                Log::error($th);
-            }
-        }
-
-        try {
-            Storage::delete($this->file);
-        } catch (\Throwable $th) {
-            Log::error($th);
-        }
-    }
-     /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handleProfessor()
-    {
-        //sleep(10);
-        $storagePath = Storage::disk('local')->getDriver()->getAdapter()->getPathPrefix();
-        $file = fopen("$storagePath/$this->file", 'r');
-
-        $i = 0;
-        $data = array();
-        while (($dataCSV = fgetcsv($file, 1000, "\t")) !== FALSE) {
-            for ($c=0; $c < count($dataCSV); $c++) {
-                $data[$i][] = $dataCSV[$c];
-            }
-            
-            $i++;
-        }
-        fclose($file);
-
-        $i = 1;
-        $array = array();
-        foreach ($data as $employee) {
-            try {
-                $employee = explode(';', $employee[0]);
-                $_employee = new User;
-                $_employee->firstname = $employee[0];
-                $_employee->lastname = $employee[1];
-                $_employee->username = $employee[2];
-                $_employee->dni = $employee[3];
-                $_employee->email = $employee[4];
-                $_employee->password = Hash::make($employee[5]);
-                $_employee->id_city = CityController::getIdFromPostalCode($employee[6]);
-                $_employee->id_role = 4;
-                $_employee->status = "active";
-                array_push($array, $_employee);
-            } catch (\ErrorException $th) {
-                Log::error($th);
-            }
-            $i++;
-        }
-
-        
-        foreach ($array as $data) {
-            try {
-                $data->save();
-                if ($data instanceof User) {
-                    switch ($data->id_role) {
-                        case '2':
+                        case '4':
                             Log::info($this->byuser->username . ' - [ IMPORT ] - users - Nou professor: ' . $data->username . ' inserit!');
+
                             break;
-                        
                         default:
                             break;
                     }
